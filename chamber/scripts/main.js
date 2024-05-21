77a3dbc5dec7678c379c1a20414a55dabf23348b
DRausseo <145221251+DRausseo@users.noreply.github.com>
2024-05-21 11:35:57 -0300

    Update main.js

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,8 +1,17 @@
+/* jshint esversion: 6 */
+
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('dark-mode-toggle');
     const main = document.querySelector('main');
 
-    darkModeToggle.addEventListener('click', () => {
-        main.classList.toggle('dark-mode');
-    });
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            main.classList.toggle('dark-mode');
+        });
+    }
+
+    const lastModifiedElement = document.getElementById("lastModified");
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = document.lastModified;
+    }
 });
