c4cf83b4125f91c82b97ced81a4fe47b5157755c
DRausseo <145221251+DRausseo@users.noreply.github.com>
2025-03-14 22:30:53 -0300

    Update lazyload.js
    actualizando el codigo

--- week03/scripts/lazyload.js
+++ week03/scripts/lazyload.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll("img");
-    
+
     images.forEach(img => {
         img.addEventListener("load", () => {
             img.classList.add("lazy-loaded");
         });
     });
-    
+
     document.getElementById("lastModified").textContent = document.lastModified;
 });
